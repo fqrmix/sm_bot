1ae8ec439a81320ff7aa4eb09915a024a32265de
fqrmix <semen.cavallini@gmail.com>
2022-10-05 19:34:42 +0300

    Fixed CSR decoder issue (#8)

--- src/sm_bot/handlers/bot/message/base/csrdecoder.py
+++ src/sm_bot/handlers/bot/message/base/csrdecoder.py
@@ -6,56 +6,57 @@ from telebot import TeleBot, types
 import re
 
 def handle_csr_request(message: types.Message, bot: TeleBot):
-    warning_list = list()
-    try:
-        logger.info(f'[csr-decoder] User {message.from_user.id} start CSR decoder')
+    if message.chat.type == 'private':
+        warning_list = list()
         chat_id = message.chat.id
-        file_info = bot.get_file(message.document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        logger.info('[csr-decoder] Checking CSR...')
+        try:
+            logger.info(f'[csr-decoder] User {message.from_user.id} start CSR decoder')
+            file_info = bot.get_file(message.document.file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+            logger.info('[csr-decoder] Checking CSR...')
 
-        if csr_validation(downloaded_file, file_info):
-            req = load_certificate_request(FILETYPE_PEM, downloaded_file)
-            key = req.get_pubkey()
-            key_type = 'RSA' if key.type() == OpenSSL.crypto.TYPE_RSA else 'DSA'
-            subject = req.get_subject()
-            components = dict(subject.get_components())
-            str_components = convert(components)
-            bot.reply_to(message, f"Common name: {str_components['CN']}\n"
-                                  f"Organisation: {str_components['O']}\n"
-                                  f"State/province: {str_components['ST']}\n"
-                                  f"Country: {str_components['C']}\n"
-                                  f"Key algorithm: {key_type}\n"
-                                  f"Key size: {key.bits()}"
-            )
-            cert_str = convert(downloaded_file)
-            inline_cert_str = '`' + cert_str + '`'
-            bot.send_message(chat_id, inline_cert_str, parse_mode='Markdown')
-            if key.bits() != 2048:
-                warning_list.append(CsrWarning(
-                        'Длина ключа не соответствует стандарту! '\
-                        'Она должна быть равна 2048 битам'
+            if csr_validation(downloaded_file, file_info):
+                req = load_certificate_request(FILETYPE_PEM, downloaded_file)
+                key = req.get_pubkey()
+                key_type = 'RSA' if key.type() == OpenSSL.crypto.TYPE_RSA else 'DSA'
+                subject = req.get_subject()
+                components = dict(subject.get_components())
+                str_components = convert(components)
+                bot.reply_to(message, f"Common name: {str_components['CN']}\n"
+                                    f"Organisation: {str_components['O']}\n"
+                                    f"State/province: {str_components['ST']}\n"
+                                    f"Country: {str_components['C']}\n"
+                                    f"Key algorithm: {key_type}\n"
+                                    f"Key size: {key.bits()}"
+                )
+                cert_str = convert(downloaded_file)
+                inline_cert_str = '`' + cert_str + '`'
+                bot.send_message(chat_id, inline_cert_str, parse_mode='Markdown')
+                if key.bits() != 2048:
+                    warning_list.append(CsrWarning(
+                            'Длина ключа не соответствует стандарту! '\
+                            'Она должна быть равна 2048 битам'
+                        )
                     )
-                )
-            if not re.search(r"^/business/", str_components['CN']):
-                warning_list.append(CsrWarning(
-                        'Common Name не соответствует стандарту!'\
-                        ' Начало у параметра должно содержать "/business/"'
+                if not re.search(r"^/business/", str_components['CN']):
+                    warning_list.append(CsrWarning(
+                            'Common Name не соответствует стандарту!'\
+                            ' Начало у параметра должно содержать "/business/"'
+                        )
                     )
-                )
-            if len(warning_list) > 0:
-                warning_text = str()
-                for warning in warning_list:
-                    warning_text += warning.__str__() + '\n\n'
-                bot.send_message(
-                chat_id=chat_id,
-                text=f"⚠️Внимание!⚠️ Есть потенциальные ошибки при создании запроса:\n"\
-                    f"*{warning_text}*",
-                parse_mode='Markdown'
-                )
-                logger.info(f'[csr-decoder] There is the warnings found: {warning_text}')
-            logger.info('[csr-decoder] CSR decoding was successfull!')
-    except Exception as e:
-        bot.reply_to(message, e)
-        logger.error(f'[csr-decoder] {e}')
+                if len(warning_list) > 0:
+                    warning_text = str()
+                    for warning in warning_list:
+                        warning_text += warning.__str__() + '\n\n'
+                    bot.send_message(
+                    chat_id=chat_id,
+                    text=f"⚠️Внимание!⚠️ Есть потенциальные ошибки при создании запроса:\n"\
+                        f"*{warning_text}*",
+                    parse_mode='Markdown'
+                    )
+                    logger.info(f'[csr-decoder] There is the warnings found: {warning_text}')
+                logger.info('[csr-decoder] CSR decoding was successfull!')
+        except Exception as e:
+            bot.reply_to(message, e)
+            logger.error(f'[csr-decoder] {e}')
 
