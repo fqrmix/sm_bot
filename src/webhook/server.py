dcc98016b50d8cbc2f5ca7c6436308b0db2f67c4
fqrmix <semen.cavallini@gmail.com>
2022-11-15 16:37:45 +0300

    added test prints

--- src/webhook/server.py
+++ src/webhook/server.py
@@ -14,6 +14,8 @@ class WebhookServer(object):
         try:
             for current_subnet in self.trusted_subnet_list:
                 if ipaddress.ip_address(current_ip) not in current_subnet:
+                    print(current_subnet)
+                    print('Not checked!')
                     raise cherrypy.HTTPError(403, "Forbidden")
             return True
         except cherrypy.CherryPyException:
@@ -25,6 +27,7 @@ class WebhookServer(object):
         try:
             remote_ip = cherrypy.request.remote.ip
             if self.is_trusted(remote_ip):
+                print('IP is trusted!')
                 length = int(cherrypy.request.headers['content-length'])
                 json_string = cherrypy.request.body.read(length).decode("utf-8")
                 update = telebot.types.Update.de_json(json_string)
