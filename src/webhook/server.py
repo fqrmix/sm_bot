7accc03af6680427843a0c4b64740ad74a520689
fqrmix <semen.cavallini@gmail.com>
2022-11-15 16:33:32 +0300

    is_trusted method in WebhookServer class added

--- src/webhook/server.py
+++ src/webhook/server.py
@@ -5,19 +5,30 @@ import telebot
 class WebhookServer(object):
     def __init__(self, bot: telebot.TeleBot) -> None:
         self.bot = bot
+        self.trusted_subnet_list = [
+            ipaddress.ip_network('149.154.160.0/20'),
+            ipaddress.ip_network('91.108.4.0/22')
+        ]
+    
+    def is_trusted(self, current_ip):
+        try:
+            for current_subnet in self.trusted_subnet_list:
+                if ipaddress.ip_address(current_ip) not in current_subnet:
+                    raise cherrypy.HTTPError(403, "Forbidden")
+            return True
+        except cherrypy.CherryPyException:
+            return False
 
     @cherrypy.expose
     def index(self):
         print(cherrypy.request)
         try:
             remote_ip = cherrypy.request.remote.ip
-            if ipaddress.ip_address(remote_ip) not in ipaddress.ip_network('149.154.160.0/20') \
-                and ipaddress.ip_address(remote_ip) not in ipaddress.ip_network('91.108.4.0/22'):
-                raise cherrypy.HTTPError(403, "Forbidden")
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            self.bot.process_new_updates([update])
-            return ''
+            if self.is_trusted(remote_ip):
+                length = int(cherrypy.request.headers['content-length'])
+                json_string = cherrypy.request.body.read(length).decode("utf-8")
+                update = telebot.types.Update.de_json(json_string)
+                self.bot.process_new_updates([update])
+                return ''
         except cherrypy.CherryPyException:
             raise cherrypy.HTTPRedirect("https://fqrmix.ru")
